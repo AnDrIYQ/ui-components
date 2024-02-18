a1c2538157a129f67ccd2b1d6b3685739e967fec
Andrii Yurchuk <andrexonerhg@gmail.com>
2024-02-18 17:26:43 +0200

    MC-1 Initiated vue project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  Just root
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'App',
+});
+</script>
+
+<style lang="scss">
+#app {
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
